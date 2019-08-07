1bafabb01c3a6eff7943ffa603d657fc17ccd3ec
Garland Lai <garlandlai24@gmail.com>
2019-08-06 19:57:31 -0700

    add validation to songs new.html and partials to albums and songs

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -5,3 +5,10 @@ describe Album do
   it { should validate_presence_of :name }
   it { should validate_length_of(:name).is_at_most(100) }
 end
+
+describe Album do
+  it("titleizes the name of an album") do
+    album = Album.create({name: "giant steps", genre: "jazz"})
+    expect(album.name()).to(eq("Giant Steps"))
+  end
+end
