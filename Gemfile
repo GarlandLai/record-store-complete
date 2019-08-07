17edab1776ca0f7350c63fcb1993126bb2bd4686
Garland Lai <garlandlai24@gmail.com>
2019-08-06 20:50:51 -0700

    add integration testing

--- Gemfile
+++ Gemfile
@@ -61,4 +61,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 group :test do
   gem 'shoulda-matchers'
+  gem 'capybara'
 end
--- app/models/album.rb
+++ app/models/album.rb
@@ -2,12 +2,16 @@ class Album < ApplicationRecord
   has_many :songs, dependent: :destroy
   has_many :album_artists
   has_many :artists, :through => :album_artists
+
   validates :name, presence: true
   validates_length_of :name, maximum: 100
 
+  # scope :rock, -> ( where(genre: "Rock"))
+  # The scope method runs when the class is loaded that dynamically creates a method on the class. In this case, scope(:rock) creates an Album.rock method that returns all rock albums.
+
   before_save(:titleize_album)
 
-private
+  private
   def titleize_album
     self.name = self.name.titleize
   end
